f2ede56af8351959cfa36988249bafc05800b1f6
Vladimir Kozlov <postwsz@mail.ru>
2026-08-28 21:52:08 +0300

    ДЗ по теме "Введение в веб-программирование с использованием Spring Boot" выпоолнено

---
+++ src/main/java/org/skypro/skyshop/model/article/Article.java
@@ -0,0 +1,71 @@
+package org.skypro.skyshop.model.article;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.skypro.skyshop.model.search.Searchable;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class Article implements Searchable {
+    private final String title;
+    private final String text;
+    private final UUID id;
+
+    public Article(UUID id, String title, String text) {
+        this.title = title;
+        this.text = text;
+        this.id = id;
+    }
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    @JsonIgnore
+    public String getSearchTerm() {
+        return toString();
+    }
+
+    @Override
+    @JsonIgnore
+    public String getType() {
+        return "ARTICLE";
+    }
+
+    @Override
+    public String getName() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return title + "\n" + text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Article article = (Article) o;
+        return Objects.equals(title, article.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
+}
